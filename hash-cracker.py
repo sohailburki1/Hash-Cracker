8d08e5d1efbaf56e8cb4c22a96d74e851b622476
Muhammad Sohail <sohailburki76@gmail.com>
2024-10-19 15:48:22 +0500

    Added hash-cracker

---
+++ hash-cracker.py
@@ -0,0 +1,40 @@
+import hashlib
+
+def main():
+    # Example hash: '5f4dcc3b5aa765d61d8327deb882cf99' (MD5 of 'password')
+    target_hash = input("Enter the hash to crack: ")
+    wordlist = input("Path to wordlist: ")
+    hash_type = input("Enter the hash type (md5, sha1, sha256, sha512): ").lower()
+
+    crack_hash(target_hash, wordlist, hash_type)
+
+
+def crack_hash(hash_to_crack, wordlist_file, hash_type='md5'):
+    try:
+        with open(wordlist_file, 'r') as file:
+            for word in file.readlines():
+                word = word.strip()
+                print(f"Trying password: {word}")
+
+                if hash_type == 'md5':
+                    hashed_word = hashlib.md5(word.encode()).hexdigest()
+                elif hash_type == 'sha256':
+                    hashed_word = hashlib.sha256(word.encode()).hexdigest()
+                elif hash_type == 'sha1':
+                    hashed_word = hashlib.sha1(word.encode()).hexdigest()
+                elif hash_type == 'sha512':
+                    hashed_word = hashlib.sha512(word.encode()).hexdigest()
+                else:
+                    print("Unsupported hash type!")
+                    return
+
+                if hashed_word == hash_to_crack:
+                    print(f"Hash cracked! The password is: {word}")
+                    return
+
+            print("Password not found in wordlist.")
+    except FileNotFoundError:
+        print("Wordlist file not found. Please check the file path.")
+
+if __name__ == '__main__':
+    main()
